58b709e85e80b46ef3ae6eee364d800ec00d114b
Charmaine Lee <charmainekmlee@yahoo.ca>
2016-10-16 02:40:10 -0700

    day 1

--- Day1.java
+++ Day1.java
@@ -13,25 +13,20 @@ public class Solution {
 		
         Scanner scan = new Scanner(System.in);
 
- /* Declare second integer, double, and String variables. */
         int integ;
         double num;
         String str;
 
-        /* Read and save an integer, double, and String to your variables.*/
         integ = scan.nextInt();
         num = scan.nextDouble();
         str = scan.next();
-        str += scan.nextLine(); // scan.next() only outputs "is", need this concatenation to append to next line
+        str += scan.nextLine(); 
 
-        /* Print the sum of both integer variables on a new line. */
         i = i + integ;
         System.out.println(i);
-        /* Print the sum of the double variables on a new line. */
         d = d + num;
         System.out.println(d);		
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
+	    
         String combo = s.concat(str);
         System.out.println(combo);		
 
